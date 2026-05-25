138ec2ec63d6a5f082c2bcbd067208a9efe9ce4c
Tomasz <cierachowski_tlw@wp.pl>
2026-05-25 20:08:10 +0200

    Add build param method and code refactor

--- app/services/rawg_client.py
+++ app/services/rawg_client.py
@@ -14,12 +14,19 @@ class Settings(BaseSettings):
 settings = Settings()
 
 
-async def get_games(api_key=settings.rawg_api_key, timeout=10.0):
-    if not api_key:
-        raise RuntimeError("Brak API KEY")
+def build_auth_params() -> dict[str, str]:
+    return {"key": settings.rawg_api_key}
 
-    async with httpx.AsyncClient(base_url=settings.rawg_base_url) as client:
-        response = await client.get("/games", params={"key": settings.rawg_api_key})
+
+async def get_games(timeout: float = 10.0):
+    if not settings.rawg_api_key:
+        raise RuntimeError("No api key to load")
+
+    async with httpx.AsyncClient(
+        base_url=settings.rawg_base_url,
+        timeout=timeout,
+    ) as client:
+        response = await client.get("/games", params=build_auth_params())
         response.raise_for_status()
         return response.json()
 
@@ -28,8 +35,6 @@ async def main():
     try:
         data = await get_games()
         print("All games count:", data["count"])  # 899298
-        # for game in data['results']:
-        #    print(game['name'], game['rating'])
 
     except httpx.HTTPStatusError as error:
         print("HTTP error:", error.response.status_code, error.response.text)
