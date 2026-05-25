b5c645ca517693d1457b1ad0d4d132123b8841bd
Tomasz <cierachowski_tlw@wp.pl>
2026-05-25 19:32:41 +0200

    Removed .dotenv and added pydantic settings for .env file

--- app/services/rawg_client.py
+++ app/services/rawg_client.py
@@ -2,19 +2,28 @@ import asyncio
 import os
 
 import httpx
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()
 
 API_KEY = os.getenv("RAWG_API_KEY")
 RAWG_BASE_URL = "https://api.rawg.io/api"
 
 
-async def get_games(api_key=API_KEY, timeout=10.0):
+class Settings(BaseSettings):
+    rawg_api_key: str
+    rawg_base_url: str
+
+    model_config = SettingsConfigDict(
+        env_file= ".env"
+    )
+
+settings = Settings()
+
+async def get_games(api_key=settings.rawg_api_key, timeout=10.0):
     if not api_key:
         raise RuntimeError("Brak API KEY")
 
-    async with httpx.AsyncClient(base_url=RAWG_BASE_URL) as client:
+    async with httpx.AsyncClient(base_url=settings.rawg_base_url) as client:
         response = await client.get("/games", params={"key": api_key})
         response.raise_for_status()
         return response.json()
