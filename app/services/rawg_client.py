38923d32b61a0c188210f2e14902378880fa8a62
Tomasz <cierachowski_tlw@wp.pl>
2026-05-25 19:51:06 +0200

    Last commit refactor

--- app/services/rawg_client.py
+++ app/services/rawg_client.py
@@ -1,30 +1,25 @@
 import asyncio
-import os
 
 import httpx
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-API_KEY = os.getenv("RAWG_API_KEY")
-RAWG_BASE_URL = "https://api.rawg.io/api"
 
 
 class Settings(BaseSettings):
     rawg_api_key: str
     rawg_base_url: str
 
-    model_config = SettingsConfigDict(
-        env_file= ".env"
-    )
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
+
 
 async def get_games(api_key=settings.rawg_api_key, timeout=10.0):
     if not api_key:
         raise RuntimeError("Brak API KEY")
 
     async with httpx.AsyncClient(base_url=settings.rawg_base_url) as client:
-        response = await client.get("/games", params={"key": api_key})
+        response = await client.get("/games", params={"key": settings.rawg_api_key})
         response.raise_for_status()
         return response.json()
 
@@ -35,13 +30,13 @@ async def main():
         print("All games count:", data["count"])  # 899298
         # for game in data['results']:
         #    print(game['name'], game['rating'])
-        
+
     except httpx.HTTPStatusError as error:
         print("HTTP error:", error.response.status_code, error.response.text)
 
     except httpx.RequestError as error:
         print("Connection error:", error)
-    
+
     except RuntimeError as error:
         print("Runtime error:", error)
 
