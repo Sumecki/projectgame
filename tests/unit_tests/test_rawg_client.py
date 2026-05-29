d85bc54cb842c05d0d10a21d7fbfa27c5370b5ea
Tomasz <cierachowski_tlw@wp.pl>
2026-05-29 19:59:42 +0200

    Add test get_game_desc_by_name when description is missing

--- tests/unit_tests/test_rawg_client.py
+++ tests/unit_tests/test_rawg_client.py
@@ -48,3 +48,19 @@ async def test_get_game_description_by_name_returns_description(rawg_client):
 
     result = await rawg_client.get_game_description_by_name("Resident Evil")
     assert result == "Game description"
+
+@pytest.mark.asyncio
+async def test_get_game_description_by_name_returns_none_when_description_missing(rawg_client):
+    rawg_client._make_request = AsyncMock(
+        side_effect=[
+            {
+                "results":[
+                    {"id": 321}
+                ]
+            },
+            {}
+        ]
+    )
+
+    result = await rawg_client.get_game_description_by_name("Diablo")
+    assert result is None
