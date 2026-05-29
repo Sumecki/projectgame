8b05bcdb526ddde065e9a99a234c927523fd77f2
Tomasz <cierachowski_tlw@wp.pl>
2026-05-29 19:46:13 +0200

    Add test get_game_desc_by_name working as expected

--- tests/unit_tests/test_rawg_client.py
+++ tests/unit_tests/test_rawg_client.py
@@ -30,3 +30,21 @@ async def test_get_game_description_by_name_returns_none_when_game_has_no_id(raw
 
     result = await rawg_client.get_game_description_by_name("witcher")
     assert result is None
+
+@pytest.mark.asyncio
+async def test_get_game_description_by_name_returns_description(rawg_client):
+    rawg_client._make_request = AsyncMock(
+        side_effect=[
+            {
+                "results": [
+                    {"id": 123}
+                ]
+            },
+            {
+                "description_raw": "Game description"
+            }
+        ]
+    )
+
+    result = await rawg_client.get_game_description_by_name("Resident Evil")
+    assert result == "Game description"
