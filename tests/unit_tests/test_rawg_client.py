d4d66c5f9ac4d8e57a800c62aa064f68eaa5a247
Tomasz <cierachowski_tlw@wp.pl>
2026-05-30 12:20:34 +0200

    Add test parametrize to merge two similiar tests

--- tests/unit_tests/test_rawg_client.py
+++ tests/unit_tests/test_rawg_client.py
@@ -7,25 +7,21 @@ from app.services.rawg_client import RawgApiClient
 def rawg_client():
     return RawgApiClient()
 
-@pytest.mark.asyncio
-async def test_get_game_description_by_name_returns_none_when_no_results(rawg_client):
-    rawg_client._make_request = AsyncMock(
-        return_value={
-            "results": []
-        }
-    )
-
-    result = await rawg_client.get_game_description_by_name("witcher")
-    assert result is None
+@pytest.mark.parametrize(
+        "search_response",
+        [
+            {"results": []},
+            {"results": [{"name": "Witcher"}]}
+        ]
+)
 
 @pytest.mark.asyncio
-async def test_get_game_description_by_name_returns_none_when_game_has_no_id(rawg_client):
+async def test_get_game_description_by_name_returns_none(
+    rawg_client,
+    search_response,
+):
     rawg_client._make_request = AsyncMock(
-        return_value={
-            "results": [
-                {"name": "Witcher"}
-            ]
-        }
+        return_value=search_response
     )
 
     result = await rawg_client.get_game_description_by_name("witcher")
