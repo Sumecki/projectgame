e34415e371b71e42451bea4055432a43feda5c46
Tomasz <cierachowski_tlw@wp.pl>
2026-06-10 16:51:09 +0200

    Revert Alembic formatting changes

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,14 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
+
 from app.core.base import Base
 from app.models.game import Game  # noqa: F401
 from app.models.user import User  # noqa: F401
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -66,7 +69,9 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
