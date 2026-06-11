e7cf39c05485d092a3bc70fa40c38bf8ad8a8050
Tomasz <cierachowski_tlw@wp.pl>
2026-06-11 19:19:16 +0200

    Add JWT authentication dependency - get_current_user function

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,10 +1,18 @@
 from datetime import UTC, datetime, timedelta
+from typing import Annotated
 
 import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+from pydantic import ValidationError
 
 from app.config import get_settings
+from app.core.domain.schemas.auth import TokenPayload
 
 settings = get_settings()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
 def create_access_token(user_email: str) -> tuple[str, float]:
@@ -20,3 +28,23 @@ def create_access_token(user_email: str) -> tuple[str, float]:
     token = jwt.encode(payload, settings.jwt_secret_key, algorithm=settings.algorithm)
 
     return token, expire.timestamp()
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+) -> TokenPayload:
+
+    try:
+        payload = jwt.decode(
+            token, settings.jwt_secret_key, algorithms=[settings.algorithm]
+        )
+        token_data = TokenPayload(**payload)
+
+    except (InvalidTokenError, ValidationError) as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from exc
+
+    return token_data
