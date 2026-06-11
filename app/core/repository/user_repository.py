9c48d685d1c9d27973a3c2a62c5f2de61b500aec
Tomasz <cierachowski_tlw@wp.pl>
2026-06-11 21:28:07 +0200

    Add UserRepository

---
+++ app/core/repository/user_repository.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+
+from app.core.domain.models.user import User
+
+
+class UserRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_user_by_email(self, email: str) -> User | None:
+        return self.session.query(User).filter(User.email == email).first()
+
+    def create_user(self, user: User) -> User:
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+
+        return user
+
+    def get_user_by_id(self, user_id: int) -> User | None:
+        return self.session.query(User).filter(User.id == user_id).first()
