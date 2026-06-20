bbb6be2f2fb38dcc9d1c4d7c6adcb46bd16a29d9
Tomasz <cierachowski_tlw@wp.pl>
2026-06-20 10:34:56 +0200

    Refactor securtiy.py uses parametrize to merge 2 tests

--- tests/unit_tests/test_security.py
+++ tests/unit_tests/test_security.py
@@ -1,4 +1,7 @@
+import pytest
+
 from app.auth.security import hash_password, verify_password
+
 
 class TestSecurityPassword:
     PASSWORD = "SecretPassword!"
@@ -14,22 +17,24 @@ class TestSecurityPassword:
 
         assert self.PASSWORD != hashed_password
 
-
-    def test_verify_password_returns_true_for_valid_password(self):
-        hashed_password = hash_password(self.PASSWORD)
-
-        assert verify_password(self.PASSWORD, hashed_password) is True
-
-
-    def test_verify_password_returns_false_for_invalid_password(self):
-        hashed_password = hash_password(self.PASSWORD)
-
-        assert verify_password(self.WRONG_PASSWORD, hashed_password) is False
-
-
-    def test_hash_password_return_different_hashes_for_same_password(self):
+    def test_hash_password_returns_different_hashes_for_same_password(self):
         first_hash = hash_password(self.PASSWORD)
         second_hash = hash_password(self.PASSWORD)
 
         assert first_hash != second_hash
-    
+
+    @pytest.mark.parametrize(
+        ("password", "expected_result"),
+        [
+            pytest.param(PASSWORD, True, id="valid-password"),
+            pytest.param(WRONG_PASSWORD, False, id="invalid-password"),
+        ],
+    )
+    def test_verify_password_returns_expected_result(
+        self,
+        password: str,
+        expected_result: bool,
+    ):
+        hashed_password = hash_password(self.PASSWORD)
+
+        assert verify_password(password, hashed_password) is expected_result
