8dbea2ea8c42746f711985bd055e0a99ac417441
Tomasz <cierachowski_tlw@wp.pl>
2026-06-23 21:50:09 +0200

    Refactor tests auth.py for new get_current_user method

--- tests/unit_tests/test_auth.py
+++ tests/unit_tests/test_auth.py
@@ -1,10 +1,10 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import jwt
 import pytest
 
 from app.auth.auth import create_access_token, get_current_user
-from app.core.domain.schemas.auth import TokenPayload
+from app.core.domain.models.user import User
 from app.core.exceptions import TokenValidationError
 
 
@@ -32,17 +32,33 @@ class TestAuth:
         assert "exp" in payload
 
     @pytest.mark.asyncio
-    async def test_get_current_user_returns_token_payload_for_valid_token(
+    async def test_get_current_user_returns_user_for_valid_token(
         self,
         patched_auth_settings: Mock,
     ):
         token, _expire = create_access_token(self.EMAIL)
 
-        token_payload = await get_current_user(token)
+        db = Mock()
 
-        assert isinstance(token_payload, TokenPayload)
-        assert token_payload.sub == self.EMAIL
-        assert token_payload.exp > 0
+        user = User(
+            username="John",
+            email=self.EMAIL,
+            hashed_password="hashed-password",
+        )
+
+        with patch("app.auth.auth.UserRepository") as user_repository_class_mock:
+            with patch("app.auth.auth.UserService") as user_service_class_mock:
+                user_service_instance_mock = user_service_class_mock.return_value
+                user_service_instance_mock.get_user_by_email.return_value = user
+
+                current_user = await get_current_user(token, db)
+
+        assert current_user is user
+        user_repository_class_mock.assert_called_once_with(db)
+        user_service_class_mock.assert_called_once_with(
+            user_repository_class_mock.return_value,
+        )
+        user_service_instance_mock.get_user_by_email.assert_called_once_with(self.EMAIL)
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
@@ -60,6 +76,6 @@ async def test_get_current_user_raises_error_for_invalid_token(
     token = request.getfixturevalue(token_fixture_name)
 
     with pytest.raises(TokenValidationError) as exc_info:
-        await get_current_user(token)
+        await get_current_user(token, Mock())
 
     assert str(exc_info.value) == "Could not validate credentials"
