d9c7624e3139c341bf1086f6291e75b3e0758afb
Tomasz <cierachowski_tlw@wp.pl>
2026-06-25 21:34:09 +0200

    Refactored user model to generate user id with default factory

--- app/core/domain/models/user.py
+++ app/core/domain/models/user.py
@@ -10,9 +10,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        default=uuid.uuid4,
+        UUID(as_uuid=True), primary_key=True, default_factory=uuid.uuid4, init=False
     )
 
     username: Mapped[str] = mapped_column(unique=True)
