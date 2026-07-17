dc82a0d5bd5b8e0fcf885978754b1d2dc68cccfc
Tomasz <cierachowski_tlw@wp.pl>
2026-07-17 17:57:21 +0200

    Add test for custom page size param in rawg_client, fix other test params and assertion

--- tests/unit_tests/test_rawg_client.py
+++ tests/unit_tests/test_rawg_client.py
@@ -59,6 +59,29 @@ class TestRawgApiClient:
         )
 
     @pytest.mark.asyncio
+    async def test_search_games_passes_custom_page_size(
+        self,
+        rawg_client,
+    ):
+        rawg_client._make_request = AsyncMock(
+            return_value ={"results":[]}
+        )
+
+        await rawg_client.search_games(
+            "counter strike",
+            page_size=5,
+        )
+
+        rawg_client._make_request.assert_awaited_once_with(
+            method="GET",
+            path="/games",
+            params={
+                "search": "counter strike",
+                "page_size": 5,
+            }
+        )
+
+    @pytest.mark.asyncio
     async def test_get_game_calls_make_request_with_correct_arguments(
         self, rawg_client
     ):
@@ -123,7 +146,10 @@ class TestRawgApiClient:
             await rawg_client._make_request(
                 method="GET",
                 path="/games",
-                params={"search": "witcher"},
+                params={
+                    "search": "witcher",
+                    "page_size": 10,
+                },
             )
 
         mock_client.request.assert_awaited_once_with(
@@ -132,6 +158,7 @@ class TestRawgApiClient:
             params={
                 "key": rawg_client.api_key,
                 "search": "witcher",
+                "page_size": 10,
             },
         )
 
